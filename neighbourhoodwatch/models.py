f390809a7676cc7fea32daaefda58aed00ef2949
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2020-02-14 10:33:56 +0300

    first commit

---
+++ neighbourhoodwatch/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from PIL import Image
+from django.urls import reverse
+from django.contrib.auth.models import 
+from pyuploadcare.dj.models import ImageField
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default="default.jpg", upload_to="pictures")
+    neighbourhood= models.CharField(max_length=70)
+    city = models.CharField(max_length=70)
+    country = models.CharField(max_length=70)
+
+class Post(models.Model):
+    image = models.ImageField(default="default.jpg", upload_to="pictures")
+    title = models.CharField(max_length=70)
+    message = models.TextField(max_length=200)
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=25)
+    location = models.CharField(max_length=25)
+    population = models.IntegerField()
+
+class Business(models.Model):
+    name = models.CharField(max_length=25)
+    neighbourhood = models.ForeignKey()
+    email = models.EmailField(max_length=250)
