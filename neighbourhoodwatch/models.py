0bb34132482fc204d425243fd7b1cb97537b28bc
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2020-02-16 23:10:22 +0300

    edited confs for models.py

--- neighbourhoodwatch/models.py
+++ neighbourhoodwatch/models.py
@@ -8,33 +8,29 @@ from pyuploadcare.dj.models import ImageField
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default="default.jpg", upload_to="pictures")
-    neighbourhood= models.CharField(max_length=70)
-    city = models.CharField(max_length=70)
-    country = models.CharField(max_length=70)
 
 class Post(models.Model):
     image = models.ImageField(default="default.jpg", upload_to="pictures")
     title = models.CharField(max_length=70)
     message = models.TextField(max_length=200)
     post_date = models.DateTimeField(auto_now_add=True)
-
+    masterpost = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
+    
     @classmethod
     def show_posts(cls):
         return cls.objects.order("post_date")[::1]
 
-class Neighbourhood(models.Model):
-    name = models.CharField(max_length=25)
-    city = models.CharField(max_length=25)
-    country = models.CharField(max_length=25)
-    population = models.IntegerField()
-    masterkey = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Business(models.Model):
     name = models.CharField(max_length=25)
-    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
     email = models.EmailField(max_length=250)
-    
     @classmethod
     def search_businesses_by_title(cls,search):
         return cls.objects.filter(name__icontains=search)
 
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=25)
+    population = models.IntegerField()
+    masterneighbourhood = models.ForeignKey(User, on_delete=models.CASCADE)
+    business = models.ManyToManyField(Business)
